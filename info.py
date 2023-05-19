2d7467aa561b8c606aa9165a3463bc2d25ddbc5c
kopaing2038 <126549210+kopaing2038@users.noreply.github.com>
2023-05-19 19:28:08 +0630

    Update info.py

--- info.py
+++ info.py
@@ -21,7 +21,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN','5532990897:AAGDd9X1ifFbcwm9PcLzuCzYKlYWHZUV
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 
-PICS = (environ.get('PICS', 'https://graph.org/file/ca4c3909ae03629e01a35.jpg')).split()
+PICS = (environ.get('PICS', 'https://graph.org/file/8301eabe638567d8f96f8.jpg https://graph.org/file/5a9647f73bfbf7c7711d9.jpg')).split()
 NOR_IMG = environ.get("NOR_IMG", "https://telegra.ph/file/46443096bc6895c74a716.jpg")
 MELCOW_VID = environ.get("MELCOW_VID", "https://telegra.ph/file/451f038b4e7c2ddd10dc0.mp4")
 SPELL_IMG = environ.get("SPELL_IMG", "https://telegra.ph/file/5e2d4418525832bc9a1b9.jpg")
