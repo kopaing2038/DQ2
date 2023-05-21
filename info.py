397707f1ea8c8337f028ee4ddd47976d52512997
kopaing2038 <126549210+kopaing2038@users.noreply.github.com>
2023-05-21 22:43:05 +0630

    Update info.py

--- info.py
+++ info.py
@@ -42,7 +42,7 @@ SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://ilfilms:ilfilms@cluster1.wteyv7d.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://pmbot1:pmbot1@cluster0.esuavhf.mongodb.net/?retryWrites=true&w=majority")
 DATABASE_NAME = environ.get('DATABASE_NAME', "KPL1")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'CHH')
 COLLECTION_NAME2 = environ.get('COLLECTION_NAME2', 'PM File')
