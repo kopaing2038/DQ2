6f69b5d2087e7057a2068ff70fcf13f8e5c40af5
kopaing2038 <126549210+kopaing2038@users.noreply.github.com>
2023-05-23 22:14:14 +0630

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -354,9 +354,9 @@ async def start(client, message):
             size=get_size(file.file_size)
             user = {message.from_user.mention}
             f_caption = f"<code>{title}</code>"
-            if CUSTOM_FILE_CAPTION:
+            if CUSTOM_FILE_CAPTION2:
                 try:
-                    f_caption=CUSTOM_FILE_CAPTION.format(file_name= '' if title is None else title, file_size='' if size is None else size, file_caption='', user_link='' if user is None else user)
+                    f_caption=CUSTOM_FILE_CAPTION2.format(file_name= '' if title is None else title, file_size='' if size is None else size, file_caption='')
                 except:
                     return
             await msg.edit_caption(f_caption)
@@ -371,7 +371,7 @@ async def start(client, message):
     f_caption=files.caption
     if CUSTOM_FILE_CAPTION:
         try:
-            f_caption=CUSTOM_FILE_CAPTION.format(file_name= '' if title is None else title, file_size='' if size is None else size, file_caption='' if f_caption is None else f_caption, user_link='' if user is None else user)
+            f_caption=CUSTOM_FILE_CAPTION2.format(file_name= '' if title is None else title, file_size='' if size is None else size, file_caption='' if f_caption is None else f_caption)
         except Exception as e:
             logger.exception(e)
             f_caption=f_caption
