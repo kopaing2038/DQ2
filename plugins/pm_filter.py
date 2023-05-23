672fc6526e5d39b04858f00a62f4605d8d17f419
kopaing2038 <126549210+kopaing2038@users.noreply.github.com>
2023-05-23 21:54:16 +0630

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -671,13 +671,15 @@ async def cb_handler(client: Client, query: CallbackQuery):
         files = files_[0]
         title = files.file_name
         size = get_size(files.file_size)
+        user = {query.from_user.mention}
         f_caption = files.caption
         settings = await get_settings(query.message.chat.id)
         if CUSTOM_FILE_CAPTION:
             try:
                 f_caption = CUSTOM_FILE_CAPTION.format(file_name='' if title is None else title,
                                                        file_size='' if size is None else size,
-                                                       file_caption='' if f_caption is None else f_caption)
+                                                       file_caption='' if f_caption is None else f_caption,
+                                                       user_link='' if size is None else user)
             except Exception as e:
                 logger.exception(e)
             f_caption = f_caption
