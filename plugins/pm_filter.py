2f77f4a29d58b08de3f7aa0213f191d97fa2989d
kopaing2038 <126549210+kopaing2038@users.noreply.github.com>
2023-06-07 11:02:48 +0630

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1737,7 +1737,7 @@ async def auto_filter(client, msg, spoll=False):
                         [
                             InlineKeyboardButton("𝐏𝐀𝐆𝐄", callback_data="pages"),
                             InlineKeyboardButton(text=f"1/{math.ceil(int(total_results)/10)}", callback_data="pages"),
-                            InlineKeyboardButton(text="𝐍𝐄𝐗𝐓 ➪", callback_data=f"next_{req}_{key}_{offset}")
+                            InlineKeyboardButton(text="𝐍𝐄𝐗𝐓 ➪", callback_data=f"#next_{req}_{key}_{offset}")
                         ]
                     )
                 else:
@@ -1745,11 +1745,12 @@ async def auto_filter(client, msg, spoll=False):
                         [
                             InlineKeyboardButton("𝐏𝐀𝐆𝐄", callback_data="pages"),
                             InlineKeyboardButton(text=f"1/{math.ceil(int(total_results)/int(MAX_B_TN))}", callback_data="pages"),
-                            InlineKeyboardButton(text="𝐍𝐄𝐗𝐓 ➪", callback_data=f"next_{req}_{key}_{offset}")
+                            InlineKeyboardButton(text="𝐍𝐄𝐗𝐓 ➪", callback_data=f"#next_{req}_{key}_{offset}")
                         ]
                     )
             except Exception as e:
                 print(f"Error: {e}")
+
     btn = btn_a + btn_b
 
 
